830ca07bfd05fda8006b243430188e7d626a3fa0
Peppermint-Nightmare <mpcoleman1@gmail.com>
2018-03-02 19:51:15 +0000

    extra MBA stuff

--- setups/laptop.nix
+++ setups/laptop.nix
@@ -11,6 +11,22 @@
   };
 
   # Touchpad
-  services.xserver.libinput.enable = true;
-  services.xserver.libinput.naturalScrolling = true;
+  services.xserver.libinput = {
+    enable = true;
+    naturalScrolling = true;
+    disableWhileTyping = true;
+  };
+
+  boot = {
+   extraModulePackages = with config.boot.kernelPackages; [ mba6x_bl ];
+   kernelModules = [ "mba6x_bl" ];
+
+   # Divides power consumption by two.
+   kernelParams = [ "acpi_osi=" ];
+ };
+
+ services.xserver.deviceSection = lib.mkDefault ''
+  Option "Backlight" "mba6x_backlight"
+  Option "TearFree" "true"
+'';
 }
